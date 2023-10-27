1019cd3c0af713a1afeea7ef325408833bc0b9f6
AxelSevenS <spaghettiandmeatballs61@gmail.com>
2023-10-27 08:41:03 +0200

    initial plugin setup

---
+++ src/main/java/fr/ludos/roles/PlayerRole.java
@@ -0,0 +1,10 @@
+package fr.ludos.roles;
+
+import org.bukkit.entity.Player;
+
+public abstract class PlayerRole {
+
+    public abstract void processCrafting(Player player);
+    public abstract void processAbilities(Player player);
+
+}
