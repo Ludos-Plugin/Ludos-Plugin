e1dc61d249ba907f76fbe0bd0b753a2769b8f250
pham <pham.guillome@gmail.com>
2025-01-23 14:32:44 +0100

    refactor: Comment out unused imports and class definitions in role classes

--- plugin/src/main/java/fr/ludos/role/StalkerRole.java
+++ plugin/src/main/java/fr/ludos/role/StalkerRole.java
@@ -1,35 +1,35 @@
-package fr.ludos.role;
+// package fr.ludos.role;
 
-import java.util.Collections;
-import java.util.Map;
+// import java.util.Collections;
+// import java.util.Map;
 
-import fr.ludos.game.Game;
-import fr.ludos.item.SpecialItem.Events;
+// import fr.ludos.game.Game;
+// import fr.ludos.item.SpecialItem.Events;
 
-public class StalkerRole extends Role {
-	public static final String id = "stalker";
+// public class StalkerRole extends Role {
+// 	public static final String id = "stalker";
 
-	public StalkerRole(Builder builder, Game game) {
-		super(builder, game);
-	}
-
-
-	@Override
-	protected Map<String, Events<?>> createItemEvents(Role.Builder builder, Game game) {
-		return Collections.emptyMap();
-	}
+// 	public StalkerRole(Builder builder, Game game) {
+// 		super(builder, game);
+// 	}
 
 
-	public static class Builder extends Role.Builder {
+// 	@Override
+// 	protected Map<String, Events<?>> createItemEvents(Role.Builder builder, Game game) {
+// 		return Collections.emptyMap();
+// 	}
 
-		@Override
-		public String getId() {
-			return id;
-		}
 
-		@Override
-		public StalkerRole build(Game.Builder builder, Game game) {
-			return new StalkerRole(this, game);
-		}
-	}
-}
+// 	public static class Builder extends Role.Builder {
+
+// 		@Override
+// 		public String getId() {
+// 			return id;
+// 		}
+
+// 		@Override
+// 		public StalkerRole build(Game.Builder builder, Game game) {
+// 			return new StalkerRole(this, game);
+// 		}
+// 	}
+// }
