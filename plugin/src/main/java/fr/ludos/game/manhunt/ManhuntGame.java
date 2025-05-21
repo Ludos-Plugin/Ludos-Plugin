5a9642ace283b3285e3cc393ef34120d13d187b4
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-05-21 21:14:53 +0200

    fix: Fix text color in game description from gray to black

--- plugin/src/main/java/fr/ludos/game/manhunt/ManhuntGame.java
+++ plugin/src/main/java/fr/ludos/game/manhunt/ManhuntGame.java
@@ -547,7 +547,7 @@ public class ManhuntGame extends Game {
 			return Component.text("A game of hide and seek.\n" +
 				"As the Prey, survive for as long as possible, while the Hunters try to find you.\n" +
 				"The Hunters possess a Compass that will update regularly to point at the Prey's position.\n")
-				.color(NamedTextColor.GRAY);
+				.color(NamedTextColor.BLACK);
 		}
 
 		public String getGameConfigUsage(CommandSender sender, Command command, String label) {
